6448bf73ed366af8f1b1de52dede3167a1025bdb
Marcellino Fernandez <loremenius@Marcellinos-MacBook-Pro.local>
2019-12-04 12:00:27 -0700

    follow along guided project

--- server.js
+++ server.js
@@ -1,12 +1,47 @@
 const express = require('express'); // importing a CommonJS module
+const helmet = require('helmet');
 
 const hubsRouter = require('./hubs/hubs-router.js');
 
 const server = express();
 
-server.use(express.json());
+function logger(req, res, next) {
+  console.log(`${req.method} to ${req.originalUrl}`);
+  next(); 
+}
 
-server.use('/api/hubs', hubsRouter);
+
+
+function gateKeeper(req, res, next) {
+  
+  const password = req.headers.password;
+
+  
+  if(password && password.toLowerCase() === "mellon") {
+    next();
+  } else {
+    res.status(401).json({ message: "wrong password" });
+  }
+}
+
+
+function checkRole(role) {
+  return function(req, res, next) {
+    if(role && role === req.headers.role) {
+      next()
+    } else {
+      res.status(403).json({ message: "insufficient role" });
+    }
+  }
+}
+
+
+server.use(helmet());
+server.use(express.json());
+server.use(logger);
+
+
+server.use('/api/hubs', helmet(), checkRole('admin'), hubsRouter); 
 
 server.get('/', (req, res) => {
   const nameInsert = (req.name) ? ` ${req.name}` : '';
@@ -16,5 +51,16 @@ server.get('/', (req, res) => {
     <p>Welcome${nameInsert} to the Lambda Hubs API</p>
     `);
 });
+
+server.get("/echo", (req, res) => {
+  res.send(req.headers);
+});
+
+
+server.get("/area51", gateKeeper, checkRole('agent'), (req, res) => {
+  res.send(req.headers);
+});
+
+
 
 module.exports = server;
